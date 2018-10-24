bc7fb9b55a056e80f22838522262736fe1f687b4
Binu Balan <binu.remote.360@gmail.com>
2018-10-24 14:03:44 +0530

    First local to aws source code sync workflow

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = (event, context, callback) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Launched through Jenkins')
+        body: JSON.stringify('Launched through Jenkins Second attempt')
     };
     callback(null,response);
 };
