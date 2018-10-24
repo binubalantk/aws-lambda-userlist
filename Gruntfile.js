cb46cdc653f661fdf52d13f9c72fd518dea9d814
Binu Balan <binu.remote.360@gmail.com>
2018-10-24 13:04:45 +0530

    Jenkins and AWS integrated

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+var grunt = require('grunt');
+grunt.loadNpmTasks('grunt-aws-lambda');
+
+grunt.initConfig({
+   lambda_invoke: {
+      default: {
+         options: {
+            file_name: 'index.js'
+         }
+      }
+   },
+   lambda_deploy: {
+      default: {
+         function: 'sigmaListUser'
+      }
+   },
+   lambda_package: {
+      default: {
+   }
+   }
+});
+grunt.registerTask('deploy', ['lambda_package', 'lambda_deploy'])
